a0bd0cd2564b4ba47e4f3a5e237dcfd1e471e61f
guildeol <glhrmcosta91@gmail.com>
2014-05-28 14:58:45 -0300

    Began developing collision for enemy

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -47,6 +47,8 @@ Enemy::~Enemy(){
 void Enemy::update(const double dt_){
     this->currentState->update();
     updatePosition(dt_);
+    std::array<bool, CollisionSide::SOLID_TOTAL> detections = detectCollision();
+    handleCollision(detections);
 }
 
 void Enemy::render(const double cameraX_, const double cameraY_){
@@ -80,4 +82,35 @@ void Enemy::changeState(const EStates state_){
     this->currentState->enter();
 }
 
-void Enemy::handleCollision(std::array<bool, CollisionSide::SOLID_TOTAL> detections_){}
+void Enemy::handleCollision(std::array<bool, CollisionSide::SOLID_TOTAL> detections_){
+    if(detections_.at(CollisionSide::SOLID_TOP)){ 
+        if((int)this->y%64 > 0){
+            this->y += 64 -(int)this->y%64 + 1; 
+            this->vy = 0.0;
+        }
+    }
+    if(detections_.at(CollisionSide::SOLID_BOTTOM)){
+        if(this->currentState == this->statesMap.at(EStates::AERIAL)){
+            this->y -= (int)(this->y + this->height)%64 - 1;
+            this->vy = 0.0;
+            changeState(EStates::PATROLLING);
+        }
+    }
+    else{
+        if(this->currentState != this->statesMap.at(EStates::AERIAL)){
+            changeState(EStates::AERIAL);
+        }
+    }
+    if(detections_.at(CollisionSide::SOLID_LEFT)){
+        this->x -= (int)(this->x + this->width)%64 + 1;
+        this->vx = 0.0;
+    }
+    if(detections_.at(CollisionSide::SOLID_RIGHT)){
+    
+//      std::cout << (int)this->x%64 << "\n";
+        if((int)this->x%64 > 0){
+            this->x += (64 - (int)this->x%64) + 1;
+            this->vx = 0.0;
+        }
+    }
+}
