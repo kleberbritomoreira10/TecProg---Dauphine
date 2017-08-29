10b09c649fc6db725b3d2d92e9f59606e1857465
Filipe Ribeiro <filiperibeirom7@gmail.com>
2017-08-28 21:22:00 -0300

    Applying style sheet in GameSave.cpp and GStateContinue.cpp
    Signed-off-by: Filipe Ribeiro <filiperibeirom7@gmail.com>

--- src/GameSave.cpp
+++ src/GameSave.cpp
@@ -4,127 +4,152 @@
 
 std::string filePath = "";
 
-GameSave::GameSave(){
+GameSave::GameSave()
+{
 
 }
 
-void GameSave::setSlot(int saveSelection_){	
-	switch(saveSelection_){
-		case this->Selection::SLOT_1:
-			this->filePath = "saveSlot1.dauphine";
+void GameSave::setSlot ( int saveSelection_ )
+{	
+	switch ( saveSelection_ )
+  {
+		case this -> Selection::SLOT_1:
+			this -> filePath = "saveSlot1.dauphine";
 		break;
 
-		case this->Selection::SLOT_2:
-			this->filePath = "saveSlot2.dauphine";
+		case this -> Selection::SLOT_2:
+			this -> filePath = "saveSlot2.dauphine";
 		break;
 
-		case this->Selection::SLOT_3:
-			this->filePath = "saveSlot3.dauphine";
+		case this -> Selection::SLOT_3:
+			this -> filePath = "saveSlot3.dauphine";
 		break;
 	}
 }
 
-void GameSave::createSave(){
-	
-	this->saveFile.open(this->filePath.c_str());	
+void GameSave::createSave()
+{	
+	this -> saveFile.open ( this -> filePath.c_str() );	
 
-	if(!this->saveFile.fail()){
-		this->saveFile << "-1" << std::endl;
-		this->saveFile.close();
+	if ( !this -> saveFile.fail() )
+  {
+		this -> saveFile << "-1" << std::endl;
+		this -> saveFile.close();
 	}
+
 	else
-		Log(DEBUG) << "Could not create save file at " + this->filePath;
+  {
+		Log(DEBUG) << "Could not create save file at " + this -> filePath;
+  }
 
 	return;
 }
 
-void GameSave::saveLevel(unsigned int level_, Player* player, std::vector <Enemy*> enemies, unsigned slot_){
+void GameSave::saveLevel ( unsigned int level_, Player* player, std::vector <Enemy*> enemies, unsigned slot_ )
+{
 
-	this->setSlot(slot_);	
+	this -> setSlot(slot_);	
 
-	this->saveFile.open(this->filePath.c_str());
+	this -> saveFile.open( this -> filePath.c_str() );
 
 	Log(DEBUG) << "Saved from level " << level_;
-	Log(DEBUG) << "Saved on file " << this->filePath;	
+	Log(DEBUG) << "Saved on file " << this -> filePath;	
 
-	if(!this->saveFile.fail()){
-		this->currentLevel = level_;
-		this->saveFile << this->currentLevel << std::endl;
-		this->saveFile << player->x << std::endl;
-		this->saveFile << player->y << std::endl;
-		this->saveFile << enemies.size() << std::endl;
-		for(auto enemy : enemies){
-			this->saveFile << enemy->isDead() << " ";
+	if(!this -> saveFile.fail())
+  {
+		this -> currentLevel = level_;
+		this -> saveFile << this -> currentLevel << std::endl;
+		this -> saveFile << player -> x << std::endl;
+		this -> saveFile << player -> y << std::endl;
+		this -> saveFile << enemies.size() << std::endl;
+	
+  	for ( auto enemy : enemies )
+    {
+			this -> saveFile << enemy -> isDead() << " ";
 		}
 		
-		this->saveFile.close();
+		this -> saveFile.close();
 	}
+
 	else
-		Log(DEBUG) << "Could not open save file at " + this->filePath;
+  {
+		Log(DEBUG) << "Could not open save file at " + this -> filePath;
+  }
 }
 
-int GameSave::getSavedLevel(int continueSelection_){
-	this->saveSelection = continueSelection_;
+int GameSave::getSavedLevel ( int continueSelection_ )
+{
+	this -> saveSelection = continueSelection_;
 	
 	std::string level = "-1";
 
-	if(this->saveSelection == 0){
-		this->continueFile.open("saveSlot1.dauphine");
+	if ( this -> saveSelection == 0 )
+  {
+		this -> continueFile.open( "saveSlot1.dauphine" );
 	}
-	else if(this->saveSelection == 1){
-		this->continueFile.open("saveSlot2.dauphine");
+
+	else if ( this -> saveSelection == 1 )
+  {
+		this -> continueFile.open( "saveSlot2.dauphine" );
 	}
-	else if(this->saveSelection == 2){
-		this->continueFile.open("saveSlot3.dauphine");
+
+	else if ( this -> saveSelection == 2)
+  {
+		this -> continueFile.open ( "saveSlot3.dauphine" );
 	}
 	
-	this->continueFile >> level;
+	this -> continueFile >> level;
 
-	this->continueFile.close();
+	this -> continueFile.close();
 
 	return std::stoi(level);
 }
 
-bool GameSave::isSaved(const int saveSlot_){
+bool GameSave::isSaved( const int saveSlot_ )
+{
 
-	this->setSlot(saveSlot_);	
+	this -> setSlot( saveSlot_ );	
 
-	this->continueFile.open(this->filePath.c_str());
+	this -> continueFile.open( this -> filePath.c_str() );
 
 	std::string testSave = ""; 
 
-	this->continueFile >> testSave;
+	this -> continueFile >> testSave;
 
 	// Log(DEBUG) << "TestSave " << testSave;
 
-	this->continueFile.close();
+	this -> continueFile.close();
 
-	if(testSave == "-1"){
+	if ( testSave == "-1" )
+  {
 		// Log(WARN) << "There is NO save at slot " << 1 + saveSlot_; 
 		return false;
 	}
-	else{
+
+	else
+  {
 		// Log(WARN) << "There is a save at slot " << 1 + saveSlot_;
-		this->continueFile.close();
+		this -> continueFile.close();
 		return true;
 	}
 }
 
-void GameSave::getPlayerPosition(double& playerX_, double& playerY_, const int slot_){
-	
-	setSlot(slot_);
+void GameSave::getPlayerPosition ( double& playerX_, double& playerY_, const int slot_ )
+{	
+	setSlot( slot_ );
 
-	this->continueFile.open(filePath.c_str(), std::ios_base::in);
+	this -> continueFile.open( filePath.c_str(), std::ios_base::in );
 
-	this->continueFile >> currentLevel; 
+	this -> continueFile >> currentLevel; 
 
-	this->continueFile >> playerX_;
-	this->continueFile >> playerY_;
+	this -> continueFile >> playerX_;
+	this -> continueFile >> playerY_;
 
-	this->continueFile.close();	
+	this -> continueFile.close();	
 }
 
-bool GameSave::isEnemyDead(const int numEnemy_, const int slot_){
+bool GameSave::isEnemyDead ( const int numEnemy_, const int slot_ )
+{
 
 	double skip = 0;
 
@@ -133,35 +158,39 @@ bool GameSave::isEnemyDead(const int numEnemy_, const int slot_){
 
 	bool rc = false;
 
-	setSlot(slot_);
+	setSlot( slot_ );
 
-	this->continueFile.open(filePath.c_str(), std::ios_base::in);
+	this -> continueFile.open( filePath.c_str(), std::ios_base::in );
 
-	this->continueFile >> skip;
-	this->continueFile >> skip;
-	this->continueFile >> skip;
+	this -> continueFile >> skip;
+	this -> continueFile >> skip;
+	this -> continueFile >> skip;
 
-	this->continueFile >> totalEnemies;
+	this -> continueFile >> totalEnemies;
 
 	// Log(DEBUG) << "Total Enemies on Level " << totalEnemies;
 
-	for(int i = 0; i < totalEnemies; i++){
+	for ( int i = 0; i < totalEnemies; i++ )
+  {
 
 		// Log(DEBUG) << "Is Enemy " << i << " dead?";		
 		
-		this->continueFile >> currentEnemy;
+		this -> continueFile >> currentEnemy;
 
 		// Log(DEBUG) << "Enemy under test dead status: " << currentEnemy;		
 
-		if(i == numEnemy_){
-			if(currentEnemy == 1)
+		if ( i == numEnemy_ )
+    {
+			if ( currentEnemy == 1 )
+      {
 				rc = true;
+      }
 
 			break;			
 		}
 	}
 
-	this->continueFile.close();	
+	this -> continueFile.close();	
 
 	// if(rc)
 	// 	Log(DEBUG) << "YES";		
