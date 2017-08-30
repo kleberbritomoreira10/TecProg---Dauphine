a3f70c080552b9e1bea251c802445dd85859326a
ebenezerandrade <ebenezer.andrade1@gmail.com>
2017-08-29 22:02:53 -0300

    Applying style sheet in BStateIdle.cpp and PStateAerial.cpp
    	- Applied Techniques
    	* Indentation and EOF with blank line.

--- src/BStateIdle.cpp
+++ src/BStateIdle.cpp
@@ -1,40 +1,48 @@
 #include "BStateIdle.h"
 #include "Logger.h"
 
-void BStateIdle::enter(){
-	Log(DEBUG) << "STATE IDLE BOSS";
-	this->boss->getAnimation()->changeAnimation(0, 0, 7, false, 1);
+void BStateIdle::enter()
+{
+	Log( DEBUG ) << "STATE IDLE BOSS";
+	this -> boss -> getAnimation() -> changeAnimation( 0, 0, 7, false, 1 );
 }
 
-void BStateIdle::exit(){
+void BStateIdle::exit()
+{
 
 }
 
-void BStateIdle::update(const double dt_){
-	((void)dt_); // Unused.
+void BStateIdle::update( const double dt_ )
+{
+	( ( void )dt_ ); // Unused.
 
-	if(this->boss->isRight && this->boss->x > 1960){
-		this->boss->isRight = false;
+	if( this -> boss -> isRight && this -> boss -> x > 1960 )
+	{
+		this -> boss -> isRight = false;
 	}
-	else if(!this->boss->isRight && this->boss->x < 300){
-		this->boss->isRight = true;
+	else if( !this -> boss -> isRight && this -> boss -> x < 300 )
+	{
+		this -> boss -> isRight = true;
 	}
 	
-	if(this->boss->isRight){
-		this->boss->move(false, true);
+	if( this -> boss -> isRight )
+	{
+		this -> boss -> move( false, true );
 	}
-	else{
-		this->boss->move(true, false);
+	else
+	{
+		this -> boss -> move( true, false );
 	}
 
-	if(this->boss->sawPlayer){
-		this->boss->changeState(Boss::BStates::ATTACK);
+	if( this -> boss -> sawPlayer )
+	{
+		this -> boss -> changeState( Boss::BStates::ATTACK );
 	}
 
 }
 
-BStateIdle::BStateIdle(Boss* const boss_) :
-	StateBoss(boss_)
+BStateIdle::BStateIdle( Boss* const boss_ ) :
+	StateBoss( boss_ )
 {
 
 }
--- src/PStateAerial.cpp
+++ src/PStateAerial.cpp
@@ -2,44 +2,49 @@
 #include "Logger.h"
 #include "Game.h"
 
-void PStateAerial::enter(){
-    this->box.x = 58;
-    this->box.y = 72;
-    this->box.w = 130;
-    this->box.h = 145;
+void PStateAerial::enter()
+{
+    this -> box.x = 58;
+    this -> box.y = 72;
+    this -> box.w = 130;
+    this -> box.h = 145;
 
-	this->player->getAnimation()->changeAnimation(4, 3, 14, false, 1.4);
-	this->player->isGrounded = false;
+	this -> player -> getAnimation() -> changeAnimation( 4, 3, 14, false, 1.4 );
+	this -> player -> isGrounded = false;
 
     Game::instance().getAudioHandler().addSoundEffect("res/audio/FX_NADINE/FALL_NADINE_01.wav");
 }
 
-void PStateAerial::exit(){
-    this->player->isClimbing = false;
+void PStateAerial::exit()
+{
+    this -> player -> isClimbing = false;
 }
 
-void PStateAerial::handleInput(const std::array<bool, GameKeys::MAX> keyStates_){
+void PStateAerial::handleInput( const std::array<bool, GameKeys::MAX> keyStates_ )
+{
 
 	// Idle
-    if(this->player->isGrounded){
-    	this->player->changeState(Player::PStates::IDLE);
+    if( this -> player -> isGrounded)
+    {
+    	this -> player -> changeState( Player::PStates::IDLE );
     	return;
     }
 
-    if(keyStates_[GameKeys::LATTACK]){
-        this->player->changeState(Player::PStates::ATTACKJUMPING);
+    if( keyStates_[ GameKeys::LATTACK ])
+    {
+        this -> player -> changeState( Player::PStates::ATTACKJUMPING );
         return;
     }
 
 	// Gravity
-	this->player->applyGravity();
+	this -> player -> applyGravity();
 
 	// Move (while on air)
-    this->player->move(keyStates_[GameKeys::LEFT], keyStates_[GameKeys::RIGHT]);
+    this -> player -> move( keyStates_[ GameKeys::LEFT ], keyStates_[ GameKeys::RIGHT ]);
 }
 
-PStateAerial::PStateAerial(Player* const player_) :
-	StatePlayer(player_)
+PStateAerial::PStateAerial( Player* const player_ ) :
+	StatePlayer( player_ )
 {
 
 }
