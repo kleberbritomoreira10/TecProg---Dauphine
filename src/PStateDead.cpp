47160ac1dcf32cc1c3028689bdee2de0a39d8b6f
MaxOliveira <maxwell_oliveira2@hotmail.com>
2017-09-11 19:38:56 -0300

    Adding comments and header in LevelFive.cpp, PStateDead.cpp, Player.cpp and StateGame.cpp

--- src/PStateDead.cpp
+++ src/PStateDead.cpp
@@ -1,7 +1,17 @@
+/* Dauphine
+* Universidade de Brasília - FGA
+* Técnicas de Programação, 2/2017
+* @PStateDead.cpp
+* The state when the player is idle.*/
+
 #include "PStateDead.h"
 #include "Logger.h"
 #include "Game.h"
 
+/**
+* Enter in the dead state.
+* @see StatePlayer::enter
+*/
 void PStateDead::enter()
 {
 	Log( DEBUG )  << "STATE DEAD";
@@ -15,18 +25,31 @@ void PStateDead::enter()
 	this -> player -> isGrounded = false;
 }
 
+/**
+* Exit the dead state.
+* @see StatePlayer::exit
+*/
 void PStateDead::exit()
 {
 
 }
 
+/**
+* Handle the user inputs.
+  @param KeyStates_: The key states to be handled.
+* @see StatePlayer::handleInput
+*/
 void PStateDead::handleInput( const std::array<bool, GameKeys::MAX> keyStates_ )
 {
 	( ( void )  keyStates_ ) ; // Unused.
 	this -> player -> applyGravity() ;
 }
 
-PStateDead::PStateDead( Player* const player_ )  :
+/**
+* The constructor.
+* @param player_ : Reference to the player.
+*/
+PStateDead::PStateDead( Player *const player_ )  :
 	StatePlayer( player_ )
 {
 
