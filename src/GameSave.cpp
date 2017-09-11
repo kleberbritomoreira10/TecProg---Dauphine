34a78ac071be2be760e8ee8e5946572e5fe90a05
Filipe Ribeiro <filiperibeirom7@gmail.com>
2017-09-11 15:55:57 -0300

    Creating comments for src/GameSave.cpp
    Signed-off-by: Filipe Ribeiro <filiperibeirom7@gmail.com>

--- src/GameSave.cpp
+++ src/GameSave.cpp
@@ -1,14 +1,30 @@
+/* Dauphine
+ * Universidade de Brasília - FGA
+ * Técnicas de Programação, 2/2017
+ * @GameSave.cpp
+ * The game save.
+ * Class that save the level in the game
+ */
+
 #include "GameSave.h"
 #include "Logger.h"
 #include <cstdlib>
 
 std::string filePath = "";
 
+/**
+ * The constructor.
+ * Initializes all the attributes.
+*/
 GameSave::GameSave()
 {
 
 }
 
+/**
+ * Sets the slot for save game.
+ * @param saveSelection_: The slot that will be allocated the save.
+*/
 void GameSave::setSlot ( int saveSelection_ )
 {	
 	switch ( saveSelection_ )
@@ -27,6 +43,10 @@ void GameSave::setSlot ( int saveSelection_ )
 	}
 }
 
+/**
+ * Creates the save file for the game.
+ * If the save fails register in log a error message.
+*/
 void GameSave::createSave()
 {	
 	this -> saveFile.open ( this -> filePath.c_str() );	
@@ -45,6 +65,14 @@ void GameSave::createSave()
 	return;
 }
 
+/**
+ * Register the current level in a save file.
+ * If the current level is one the registration should not be done.
+ * @param level_: The number of the current level of the game.
+ * @param player: The character of the game, in this method gets your position.
+ * @param enemies: The enemies of the game.
+ * @param slot_: The slot where are saving the game.
+*/
 void GameSave::saveLevel ( unsigned int level_, Player* player, std::vector <Enemy*> enemies, unsigned slot_ )
 {
 
@@ -77,6 +105,10 @@ void GameSave::saveLevel ( unsigned int level_, Player* player, std::vector <Ene
   }
 }
 
+/**
+ * Getting the number of the current level.
+ * @continueSelection_: The number of the slot where is saved the game.
+*/
 int GameSave::getSavedLevel ( int continueSelection_ )
 {
 	this -> saveSelection = continueSelection_;
@@ -105,6 +137,10 @@ int GameSave::getSavedLevel ( int continueSelection_ )
 	return std::stoi(level);
 }
 
+/**
+ * The analyzer if the game is saved.
+ * @saveSlot_: The slot where is saved the game.
+*/
 bool GameSave::isSaved( const int saveSlot_ )
 {
 
@@ -134,6 +170,12 @@ bool GameSave::isSaved( const int saveSlot_ )
 	}
 }
 
+/**
+ * Getting a position of a player in a game.
+ * @playerX: The horizontal position of the player in the game.
+ * @playerY: The vertical position of the player in the game.
+ * @slot_: The slot where the game will be saved.
+*/
 void GameSave::getPlayerPosition ( double& playerX_, double& playerY_, const int slot_ )
 {	
 	setSlot( slot_ );
@@ -148,6 +190,11 @@ void GameSave::getPlayerPosition ( double& playerX_, double& playerY_, const int
 	this -> continueFile.close();	
 }
 
+/**
+ * The analyzer if the enemy is dead.
+ * @numEnemy: The quantity of enemies near of the character.
+ * @slot_: The slot where the game will be saved.
+*/
 bool GameSave::isEnemyDead ( const int numEnemy_, const int slot_ )
 {
 
