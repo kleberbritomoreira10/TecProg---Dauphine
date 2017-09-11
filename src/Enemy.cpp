4cda6803a7f8812384a496f1924a5af7e0d83cb7
Kleber <kleberbritomoreira10@gmail.com>
2017-09-11 06:30:26 -0300

    Adding comments in Enemy.cpp
    Signed-off-by: Kleber <kleberbritomoreira10@gmail.com>

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -14,26 +14,36 @@
 #define ADD_STATE_EMPLACE(stateEnum, stateClass) this -> statesMap.emplace(stateEnum, new stateClass( this ))
 #define ADD_STATE_INSERT(stateEnum, stateClass) this -> statesMap.insert(std::make_pair<EStates, StateEnemy*>(stateEnum, new stateClass( this )));
 
-double Enemy::px = 0.0;
+//Declaring and initialing position enemy in the origin of the x axis 
+double Enemy::px = 0.0;                            
+//Declaring and initialing position enemy in the origin of the y axis
 double Enemy::py = 0.0;
+//Declaring the enemy with 3 life quantity
 unsigned int Enemy::pLife = 3;
+//Check if enemy is position vulnerable
 bool Enemy::pVulnerable = false;
+//Declaring the value for range dangerous of proximity for a enemy
 double Enemy::alertRange = 300.0;
+//Double value existing at pvulnerable 
 double Enemy::curiousRange = 600.0;
 
 Enemy::Enemy( const double x_, const double y_, const std::string& path_, const bool patrol_,
 	const double patrolLength_ ) : DynamicEntity(x_, y_, path_), originalX(x_), patrol(patrol_),
   patrolLength(patrolLength_), life(100), currentState(nullptr), animation(nullptr), statesMap(), dead(false)
 {
+	// Initialize all the states in Enemy.
 	initializeStates();
 
 	this -> speed = 3.0;
-
+  
+  //Getting information from lua script.
 	LuaScript luaEnemy("lua/Enemy.lua");
+	// Setting the level width/height.
 	this -> width = luaEnemy.unlua_get<int>("enemy.dimensions.width");
 	this -> height = luaEnemy.unlua_get<int>("enemy.dimensions.height");
 	this -> animation = new Animation(0, 0, this -> width, this -> height, 0, false);
 
+  //Condition to attribute the states map to current state.
 	if ( this -> patrol )
 	{
 		this -> currentState = this -> statesMap.at(PATROLLING);
@@ -44,14 +54,17 @@ Enemy::Enemy( const double x_, const double y_, const std::string& path_, const 
 	this -> currentState -> enter();
 }
 
+//Method to destroy the enemy
 Enemy::~Enemy()
 {
+	//Attribute null to current state
 	if ( this -> currentState != nullptr )
 	{
 		this -> currentState -> exit();
 		this -> currentState = nullptr;
 	}
-
+  
+  //Attribute null to animation
 	if ( this -> animation != nullptr )
 	{
     delete this -> animation;
@@ -61,8 +74,10 @@ Enemy::~Enemy()
 	destroyStates();
 }
 
+//Method to update characteristics of the enemy
 void Enemy::update( const double dt_)
 {	
+	//const double dt is passed as a parameter to know the position
 	this -> currentState -> update( dt_);
 	forceMaxSpeed();
 
@@ -71,18 +86,20 @@ void Enemy::update( const double dt_)
 	this -> animation -> update( this -> animationClip, dt_);
 
 	updateBoundingBox();
-
+  
+  //Array to detect collision with enemy
 	const std::array<bool, CollisionSide::SOLID_TOTAL> detections = detectCollision();
 	handleCollision(detections);
 
-	updatePosition(dt_);
-
+	updatePosition( dt_);
 }
 
-void Enemy::render(const double cameraX_, const double cameraY_)
-{
-	const double dx = this->x - cameraX_;
-	const double dy = this->y - cameraY_;
+void Enemy::render( const double cameraX_, const double cameraY_)
+{ 
+  //Used to locate the camera on the x-axis  
+	const double dx = this -> x - cameraX_;
+	//Used to locate the camera on the y-axis
+	const double dy = this -> y - cameraY_;
 
 	///////////////////////////////////////////////////////////////////////////////////////////
 	// // Actual.
@@ -100,7 +117,8 @@ void Enemy::render(const double cameraX_, const double cameraY_)
 	{
 		SDL_RendererFlip flip = getFlip();
 
-		if ( flip == SDL_FLIP_HORIZONTAL ){
+		if ( flip == SDL_FLIP_HORIZONTAL )
+		{
 			this -> sprite->render(dx - 120, dy, &this -> animationClip, false, 0.0, nullptr, flip);
 		} else {
 			  this -> sprite -> render(dx, dy, &this -> animationClip, false, 0.0, nullptr, flip);
@@ -110,7 +128,7 @@ void Enemy::render(const double cameraX_, const double cameraY_)
 
 void Enemy::initializeStates()
 {
-	
+	// Initialize all the states in Enemy here.
 	ADD_STATE_INSERT(IDLE,         EStateIdle);
 	ADD_STATE_INSERT(CURIOUS,      EStateCurious);
 	ADD_STATE_INSERT(PATROLLING,   EStatePatrolling);
@@ -122,7 +140,7 @@ void Enemy::initializeStates()
 
 void Enemy::destroyStates()
 {
-	
+	// Delete all the states in Enemy here.
 	std::map<EStates, StateEnemy*>::const_iterator it;
 	for ( it = this -> statesMap.begin(); it != this -> statesMap.end(); it++ )
 	{
@@ -130,22 +148,26 @@ void Enemy::destroyStates()
 	}
 }
 
+//Method used to exchange enemy of position
 void Enemy::changeState( const EStates state_)
-{
+{  
 	this -> currentState -> exit();
 	this -> currentState = this -> statesMap.at(state_);
 	this -> currentState -> enter();
 }
 
+//Method used to check collision in enemy
 void Enemy::handleCollision( std::array<bool, CollisionSide::SOLID_TOTAL> detections_)
-{
+{ 
+	//Collision on top
 	if ( detections_.at(CollisionSide::SOLID_TOP) )
 	{ 
 		this -> vy = 0.0;
 	}
+	//Collision on bottom
 	if ( detections_.at(CollisionSide::SOLID_BOTTOM) )
 	{
-		if( this -> currentState == this -> statesMap.at(EStates::AERIAL) || this -> currentState == this -> statesMap.at
+		if ( this -> currentState == this -> statesMap.at(EStates::AERIAL) || this -> currentState == this -> statesMap.at
 			(EStates::DEAD))
 		{
 			this -> nextY -= fmod( this -> nextY, 64.0 ) - 16.0;
@@ -168,11 +190,13 @@ void Enemy::handleCollision( std::array<bool, CollisionSide::SOLID_TOTAL> detect
 			  changeState(EStates::AERIAL);
 		  }
 	  }
+	//Collision on left  
 	if ( detections_.at(CollisionSide::SOLID_LEFT) )
 	{
 		this -> nextX = this -> x;
 		this -> vx = 0.0;
 	}
+	//Collision on right
 	if ( detections_.at(CollisionSide::SOLID_RIGHT) )
 	{
 		this -> nextX = this -> x;
@@ -180,6 +204,7 @@ void Enemy::handleCollision( std::array<bool, CollisionSide::SOLID_TOTAL> detect
 	}
 }
 
+//Designates maximum velocity on the x-axis and y
 void Enemy::forceMaxSpeed()
 {
 	this -> vx = ( this -> vx >= this -> maxSpeed) ? this -> maxSpeed : this -> vx ;
@@ -191,20 +216,27 @@ Animation *Enemy::getAnimation()
 	return ( this -> animation );
 }
 
+//Setting enemy is dead
 void Enemy::setDead( bool isDead_)
 {
 	this -> dead = isDead_;
 }
 
+//Inform that enemy is dead
 bool Enemy::isDead()
 {
 	return this -> dead;
 }
 
+//Method to delimit enemy dimensions update
 void Enemy::updateBoundingBox()
-{
+{ 
+	//Bounding box at x position 
 	this -> boundingBox.x = (int) this -> nextX + 40;
+	//Bounding box at y position 
 	this -> boundingBox.y = (int) this -> nextY + 40;
+	//Bounding box at width  
 	this -> boundingBox.w = 150;
+	//Bounding box at height  
 	this -> boundingBox.h = 200;
 }
